101fcc1fcb96ec83bce920cf76f9c854b3725253
ArttuOll <arttuoll.tech@tutanota.com>
2020-11-12 19:37:58 +0200

    Luo luokka SettingsWriter kirjottamaan asetukset

--- src/finance_automator.py
+++ src/finance_automator.py
@@ -3,6 +3,7 @@
 
 from sys import argv
 from .model.guided_configuration import guided_configuration
+from .model.settings_writer import SettingsWriter
 
 GUIDED_CONFIG_SHORT = "-g"
 GUIDED_CONFIG = "--guided"
@@ -10,4 +11,6 @@ GUIDED_CONFIG = "--guided"
 main_argument = argv[1]
 
 if main_argument in GUIDED_CONFIG_SHORT or main_argument in GUIDED_CONFIG:
-    guided_configuration()
+    settings = guided_configuration()
+    settings_writer = SettingsWriter(settings)
+    settings_writer.write()
