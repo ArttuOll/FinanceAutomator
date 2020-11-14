0330b785e5fe206f2c51c6427a6929077f8942a7
ArttuOll <arttuoll.tech@tutanota.com>
2020-11-14 14:59:53 +0200

    Lisää raportin kirjoittamiskomento

--- src/finance_automator.py
+++ src/finance_automator.py
@@ -5,6 +5,33 @@ from sys import argv, exit as sysexit
 from .util.guided_configuration import guided_configuration
 from .model.configs_io import ConfigsIO
 from .util.help import show_help
+from .util.write_report import write_report
+
+def handle_commands_not_requiring_settings(command):
+    """Hoitaa sellaisten komentojen tulkinnan, jotka eivät vaadi tietoa
+    ohjelman asetuksista. Tämän funktion suorittamisen jälkeen ohjelma poistuu"""
+
+    if command in GUIDED_CONFIG_SHORT or command in GUIDED_CONFIG:
+        guided_configuration()
+    elif command in HELP_SHORT or command in HELP:
+        show_help()
+
+    sysexit()
+
+
+def handle_commands_requiring_settings(configs_object, command):
+    """Hoitaa sellaisten komentojen tulkinnan, jotka vaativat tiedon ohjelman
+    asetuksista. Antaa kutsumilleen funktioille niiden vaatimat asetukset
+    argumenttina. Tätä funktiota kutsutaan vain, jos asetukset on pystytty
+    lukemaan onnistuneesti. Tämän funktion suorittamisen jälkeen ohjelma poistuu"""
+
+    if command in REPORT_SHORT or command in REPORT:
+        saving_location = configs_object["save_dir"]
+        categories_tags = configs_object["categories_tags"]
+        write_report(saving_location, categories_tags)
+
+    sysexit()
+
 
 GUIDED_CONFIG_SHORT = "-g"
 GUIDED_CONFIG = "--guided"
@@ -12,16 +39,14 @@ GUIDED_CONFIG = "--guided"
 HELP_SHORT = "-h"
 HELP = "--help"
 
+REPORT_SHORT = "-r"
+REPORT= "--report"
+
 main_argument = argv[1]
 
-if main_argument in GUIDED_CONFIG_SHORT or main_argument in GUIDED_CONFIG:
-    guided_configuration()
-    sysexit()
-elif main_argument in HELP_SHORT or main_argument in HELP:
-    show_help()
-    sysexit()
+handle_commands_not_requiring_settings(main_argument)
 
 configs_io = ConfigsIO()
 configs = configs_io.read()
 if configs:
-    print(configs)
+    handle_commands_requiring_settings(configs, main_argument)
