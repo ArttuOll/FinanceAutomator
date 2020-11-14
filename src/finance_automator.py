0353cf7f35a1253e305ce5bc8a848f5364ebeae5
ArttuOll <arttuoll.tech@tutanota.com>
2020-11-14 13:32:36 +0200

    Lisää komento aputekstin näyttämiselle

--- src/finance_automator.py
+++ src/finance_automator.py
@@ -1,17 +1,27 @@
-"""Sovelluksen pääohjelma. Sisältää käyttöliittymän ja sen riippuvuudet."""
-#!/usr/bin/env python3
+"""Sovelluksen päätiedosto. Tulkitsee komentoriviparametrit ja kutsuu niitä
+vastaavia funkioita. Lukee asetukset asetustiedostosta."""
 
 from sys import argv
 from .util.guided_configuration import guided_configuration
 from .model.configs_io import ConfigsIO
+from .util.help import show_help
 
 GUIDED_CONFIG_SHORT = "-g"
 GUIDED_CONFIG = "--guided"
 
-main_argument = argv[1]
+HELP_SHORT = "-h"
+HELP = "--help"
 
-configs_io = ConfigsIO()
-configs = configs_io.read()
+main_argument = argv[1]
 
 if main_argument in GUIDED_CONFIG_SHORT or main_argument in GUIDED_CONFIG:
     guided_configuration()
+    quit()
+elif main_argument in HELP_SHORT or main_argument in HELP:
+    show_help()
+    quit()
+
+configs_io = ConfigsIO()
+configs = configs_io.read()
+if configs:
+    # TODO: Asetuksia tarvitsevat komennot käsitellään täällä
