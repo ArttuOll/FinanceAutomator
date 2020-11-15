e734d72645d0978fdbae2b2d56dc2a5fe69de315
ArttuOll <arttuoll.tech@tutanota.com>
2020-11-15 19:45:35 +0200

    Käytä argparsea argumenttien lukemiseen
    Hoitaa kaiken argumenttien lukemisen, mikä muuten olisi pitänyt tehdä
    manuaalisesti.

--- src/finance_automator.py
+++ src/finance_automator.py
@@ -1,52 +1,43 @@
 """Sovelluksen päätiedosto. Tulkitsee komentoriviparametrit ja kutsuu niitä
 vastaavia funkioita. Lukee asetukset asetustiedostosta."""
 
-from sys import argv, exit as sysexit
+from argparse import ArgumentParser
 from .util.guided_configuration import guided_configuration
 from .model.configs_io import ConfigsIO
-from .util.help import show_help
 from .util.write_report import write_report
 
-def handle_commands_not_requiring_settings(command):
+def handle_commands_not_requiring_settings(args):
     """Hoitaa sellaisten komentojen tulkinnan, jotka eivät vaadi tietoa
     ohjelman asetuksista. Tämän funktion suorittamisen jälkeen ohjelma poistuu"""
 
-    if command in GUIDED_CONFIG_SHORT or command in GUIDED_CONFIG:
+    if args.guided:
         guided_configuration()
-    elif command in HELP_SHORT or command in HELP:
-        show_help()
-
-    sysexit()
 
 
-def handle_commands_requiring_settings(configs_object, command):
+def handle_commands_requiring_settings(configs_object, args):
     """Hoitaa sellaisten komentojen tulkinnan, jotka vaativat tiedon ohjelman
     asetuksista. Antaa kutsumilleen funktioille niiden vaatimat asetukset
     argumenttina. Tätä funktiota kutsutaan vain, jos asetukset on pystytty
     lukemaan onnistuneesti. Tämän funktion suorittamisen jälkeen ohjelma poistuu"""
 
-    if command in REPORT_SHORT or command in REPORT:
+    if args.report:
         saving_location = configs_object["save_dir"]
         categories_tags = configs_object["categories_tags"]
         write_report(saving_location, categories_tags)
 
-    sysexit()
 
+argument_parser = ArgumentParser()
+argument_parser.add_argument("-g", "--guided", action="store_true",
+                            help="Suorita ohjattu asetustiedoston luonti")
+argument_parser.add_argument("-r", "--report", action="store_true",
+                            help="""Kirjoittaa raportin viime kuukauden tilitapahtumista
+                                    tallennuskansiossa sijaitsevaan raporttitiedostoon.""")
 
-GUIDED_CONFIG_SHORT = "-g"
-GUIDED_CONFIG = "--guided"
+arguments = argument_parser.parse_args()
 
-HELP_SHORT = "-h"
-HELP = "--help"
-
-REPORT_SHORT = "-r"
-REPORT= "--report"
-
-main_argument = argv[1]
-
-handle_commands_not_requiring_settings(main_argument)
+handle_commands_not_requiring_settings(arguments)
 
 configs_io = ConfigsIO()
 configs = configs_io.read()
 if configs:
-    handle_commands_requiring_settings(configs, main_argument)
+    handle_commands_requiring_settings(configs, arguments)
