9fe4bf9e4d8e6691fa697d934e6bb819fd8e87bf
ArttuOll <arttuoll.tech@tutanota.com>
2020-11-18 19:43:12 +0200

    Lisää ominaisuus raportin tulostamiselle

--- src/util/write_report.py
+++ src/util/write_report.py
@@ -1,10 +1,12 @@
+"""Määrittelee funktion write_report ja sen riippuvuudet"""
+import json
 from datetime import datetime
 from ..model.event_extractor import EventExtractor
 from ..model.event_calculator import EventCalculator
 
-def write_report(save_dir, categories_tags):
+def write_report(save_dir, transactions_dir, categories_tags, print_output=False):
     event_extractor = EventExtractor()
-    events = event_extractor.events_from_file(save_dir)
+    events = event_extractor.events_from_file(transactions_dir)
     event_calculator = EventCalculator(events, categories_tags)
 
     values_by_category = event_calculator.calculate_values()
@@ -13,4 +15,7 @@ def write_report(save_dir, categories_tags):
     report = f"Talousraportti {datetime_now}\n"
 
     for category, value in values_by_category.items():
-        # TODO: liitä raporttimerkkijonoon
+        report += f"{category}: {value}\n"
+
+    if print_output:
+        print(report)
