8c2fe4788142c5a6f4eb4d59278431db7b37111c
ArttuOll <arttuoll.tech@tutanota.com>
2020-12-30 14:26:12 +0200

    Lisää ja täydennä käyttöliittymän aputekstejä

--- src/finance_automator.py
+++ src/finance_automator.py
@@ -18,11 +18,11 @@ def cli(context, verbose):
     context.obj.verbose = verbose
     context.obj.read()
 
-@cli.command()
+@cli.command(short_help="Suorita ohjattu asetustiedoston luominen")
 def guided_config():
     guided_configuration()
 
-@cli.command(short_help="Lukee tilitapahtumat ja kirjoittaa niistä raportin.")
+@cli.command(short_help="Lue tilitapahtumatiedosto ja kirjoita siitä raportti")
 @click.argument("transactions_file")
 @pass_configs
 def read(configs, transactions_file):
@@ -30,29 +30,28 @@ def read(configs, transactions_file):
     report_writer = ReportWriter(configs)
     report_writer.write_report()
 
-@cli.command()
+@cli.command(short_help="Laske kulu- ja menokategorioiden arvoja yhteen aikavälillä")
 @click.argument("start_date")
-@click.option("--end", help="Loppupäivämäärä")
-@click.option("--title", help="Raporttitiedoston nimi", default="")
+@click.option("--end", help="Aikavälin loppupäivämäärä")
+@click.option("--title", help="Kirjoitettavan raporttitiedoston nimi", default="")
 @pass_configs
 def sum(configs, start_date, end, title):
     report_writer = ReportWriter(configs)
     report_writer.write_sum_report(start_date, end_date=end, title=title)
 
-@cli.command()
+@cli.command(short_help="Laske kulu- ja menokategorioiden keskiarvot aikaväliä kohti")
 @click.argument("start_date")
 @click.option("--end", help="Loppupäivämäärä")
-@click.option("--title", help="Raporttitiedoston nimi", default="")
+@click.option("--title", help="Kirjoitettavan raporttitiedoston nimi", default="")
 @pass_configs
 def avg(configs, start_date, end, title):
     report_writer = ReportWriter(configs)
     report_writer.write_avg_report(start_date, end_date=end, title=title)
 
-@cli.command()
+@cli.command(short_help="Piirrä kuvaaja kategorian arvoista aikavälillä")
 @click.argument("start_date")
 @click.argument("category")
-@click.option("--end", help="Loppupäivämäärä")
-# @click.option("--title", help="Kuvaajan otsake", default="")
+@click.option("--end", help="Aikavälin loppupäivämäärä")
 @pass_configs
 def graph(configs, start_date, category, end):
     graph_drawer = GraphDrawer(configs)
